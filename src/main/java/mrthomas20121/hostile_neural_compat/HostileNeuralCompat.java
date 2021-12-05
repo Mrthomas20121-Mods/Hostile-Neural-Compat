87a00e38481d32b2513636877d45b72da4c71275
Mrthomas20121 <thomas77300@gmail.com>
2021-12-05 09:48:07 +0100

    Create HostileNeuralCompat.java

---
+++ src/main/java/mrthomas20121/hostile_neural_compat/HostileNeuralCompat.java
@@ -0,0 +1,11 @@
+package mrthomas20121.hostile_neural_compat;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(HostileNeuralCompat.MOD_ID)
+public class HostileNeuralCompat {
+
+	public static final String MOD_ID = "hostile_neural_compat";
+	//public static final Logger LOGGER = LogManager.getLogger();
+}
