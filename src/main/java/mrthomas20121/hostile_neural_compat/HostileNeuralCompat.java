cbbb20964ef3f4318ed70458b4f28afc513408e8
Mrthomas20121 <thomas77300@gmail.com>
2021-12-12 12:56:39 +0100

    Twilight Forest and Atum Compat

--- src/main/java/mrthomas20121/hostile_neural_compat/HostileNeuralCompat.java
+++ src/main/java/mrthomas20121/hostile_neural_compat/HostileNeuralCompat.java
@@ -1,6 +1,7 @@
 package mrthomas20121.hostile_neural_compat;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,5 +9,9 @@ import org.apache.logging.log4j.Logger;
 public class HostileNeuralCompat {
 
 	public static final String MOD_ID = "hostile_neural_compat";
-	//public static final Logger LOGGER = LogManager.getLogger();
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public HostileNeuralCompat() {
+		ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+	}
 }
---
+++ src/main/java/mrthomas20121/hostile_neural_compat/ItemRegistry.java
@@ -0,0 +1,30 @@
+package mrthomas20121.hostile_neural_compat;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.LazyValue;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+
+@Mod.EventBusSubscriber(modid = HostileNeuralCompat.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ItemRegistry {
+
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(Item.class, HostileNeuralCompat.MOD_ID);
+
+    public static ItemGroup group = new ItemGroup("hostile_neural_compat") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(sandy_prediction.get());
+        }
+    };
+
+    public static RegistryObject<Item> sandy_prediction = ITEMS.register("sandy_prediction", ItemRegistry::register);
+    public static RegistryObject<Item> twilight_prediction = ITEMS.register("twilight_prediction", ItemRegistry::register);
+
+    public static Item register() {
+        return new Item(new Item.Properties().tab(group));
+    }
+}
