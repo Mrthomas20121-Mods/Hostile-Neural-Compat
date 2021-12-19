cfc14d79b8b2fa318bfc1f8cac14461a73c6ab6e
Mrthomas20121 <thomas77300@gmail.com>
2021-12-19 21:31:02 +0100

    Added Savage and Ravage, Twilight Forest and Mystical World compat #1

--- src/main/java/mrthomas20121/hostile_neural_compat/ItemRegistry.java
+++ src/main/java/mrthomas20121/hostile_neural_compat/ItemRegistry.java
@@ -7,20 +7,25 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 
+import javax.annotation.Nonnull;
+
 @Mod.EventBusSubscriber(modid = HostileNeuralCompat.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ItemRegistry {
 
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(Item.class, HostileNeuralCompat.MOD_ID);
 
     public static ItemGroup group = new ItemGroup("hostile_neural_compat") {
+
+        @Nonnull
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(sandy_prediction.get());
         }
     };
 
-    public static RegistryObject<Item> sandy_prediction = ITEMS.register("sandy_prediction", ItemRegistry::register);
-    public static RegistryObject<Item> twilight_prediction = ITEMS.register("twilight_prediction", ItemRegistry::register);
+    public static final RegistryObject<Item> sandy_prediction = ITEMS.register("sandy_prediction", ItemRegistry::register);
+    public static final RegistryObject<Item> twilight_prediction = ITEMS.register("twilight_prediction", ItemRegistry::register);
+    public static final RegistryObject<Item> forgotten_prediction = ITEMS.register("forgotten_prediction", ItemRegistry::register);
 
     public static Item register() {
         return new Item(new Item.Properties().tab(group));
